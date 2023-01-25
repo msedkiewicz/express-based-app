49f3d33d8e2ec8dd5038e8c1b78a2eee4b1c8478
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-25 11:25:52 +0100

    Code formatting

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -10,30 +10,30 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.show('index.html');
+app.get("/", (req, res) => {
+  res.show("index.html");
 });
 
-app.get('/about', (req, res) => {
-  res.show('about.html');
+app.get("/about", (req, res) => {
+  res.show("about.html");
 });
 
-app.get('/contact', (req, res) => {
-  res.show('contact.html');
+app.get("/contact", (req, res) => {
+  res.show("contact.html");
 });
 
-app.get('/info', (req, res) => {
-  res.show('info.html');
+app.get("/info", (req, res) => {
+  res.show("info.html");
 });
 
-app.get('/history', (req, res) => {
-  res.show('history.html');
+app.get("/history", (req, res) => {
+  res.show("history.html");
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).send("404 not found...");
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+  console.log("Server is running on port: 8000");
 });
