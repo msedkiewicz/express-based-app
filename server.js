968f46397e7954edd343f0cc63b3e83199d82dd3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-25 20:50:28 +0100

    Adapt server.js for hbs  files

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 const hbs = require("express-handlebars");
 
-app.engine(".hbs", hbs());
-app.set("view engine", ".hbs");
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+const app = express();
+app.engine("hbs", hbs());
+app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.show("index.html");
+  res.render("index", { layout: false });
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { layout: false, name: req.params.name });
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about", { layout: false });
 });
 
 app.get("/contact", (req, res) => {
-  res.show("contact.html");
+  res.render("contact", { layout: false });
 });
 
 app.get("/info", (req, res) => {
-  res.show("info.html");
+  res.render("info", { layout: false });
 });
 
-app.get("/history", (req, res, next) => {
-  res.show("history.html");
-});
-
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+app.get("/history", (req, res) => {
+  res.render("history", { layout: false });
 });
 
 app.use((req, res) => {
