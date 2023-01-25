eb3905f57d1bd4d6e7239b469f09ef2d2444b460
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-25 20:45:19 +0100

    Switch off handlebars layout

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/history", (req, res, next) => {
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { name: req.params.name });
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
