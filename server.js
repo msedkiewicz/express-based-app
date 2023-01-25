9f4fa4f6f8e384c680c66f06259e520a210bc52b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-25 20:45:02 +0100

    Add hello page rendering

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get("/history", (req, res, next) => {
   res.show("history.html");
 });
 
-app.get("/hello/:name", (req, res) => {
-  res.send(`Hello ${req.params.name}`);
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
