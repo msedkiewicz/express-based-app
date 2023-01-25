38c79e3726e2c3cd534216aa3b068ac4ac7acd2a
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-25 21:02:15 +0100

    Adapt sites to use main layout

--- server.js
+++ server.js
@@ -5,31 +5,32 @@ const hbs = require("express-handlebars");
 const app = express();
 app.engine("hbs", hbs());
 app.set("view engine", "hbs");
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.render("index", { layout: false });
+  res.render("index");
 });
 
 app.get("/hello/:name", (req, res) => {
-  res.render("hello", { layout: false, name: req.params.name });
+  res.render("hello", { name: req.params.name });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { layout: false });
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", { layout: false });
+  res.render("contact");
 });
 
 app.get("/info", (req, res) => {
-  res.render("info", { layout: false });
+  res.render("info");
 });
 
 app.get("/history", (req, res) => {
-  res.render("history", { layout: false });
+  res.render("history");
 });
 
 app.use((req, res) => {
