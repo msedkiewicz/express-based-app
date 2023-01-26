1921ffce9c2a4417c26387eb45cf7a584e7677b8
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-26 20:10:22 +0100

    Allow x-www-form-urlencoded use

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.engine(
 );
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index");
